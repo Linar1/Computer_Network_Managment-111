d27633d9adc5291d582f87b6c8212ab52b058648
Linar1 <samig.linar@mail.ru>
2021-06-10 16:04:06 +0300

    задание 5 :
    Функция вычисляет корень линейного уравнения ax+b=0.

---
+++ linar5/linar5/root.cpp
@@ -0,0 +1,31 @@
+﻿#include "root.h"
+bool root::double_compare(const double value_a, const double value_b) const
+{
+	return abs(value_a - value_b) <= numeric_limits<double>::min();
+}
+double root::get_root() const { return (-b / a); }
+double root::noexep() const { return get_root(); }
+double root::with_thow() throw() { return get_root(); }
+double root::invalid_throw() throw(invalid_argument)
+{
+	if (double_compare(a,0))
+		throw invalid_argument("деление на ноль!!!");
+	return get_root();
+}
+double root::empty() throw(empty_exeption)
+{
+	if (double_compare(a, 0))
+		throw empty_exeption();
+	return get_root();
+}double root::my_exp1() throw(my_exeption1)
+{
+	if (double_compare(a, 0))
+		throw my_exeption1("деление на ноль!!!");
+	return get_root();
+}
+double root::my_exp2() throw(my_exeption2)
+{
+	if (double_compare(a, 0))
+		throw my_exeption2("неверный аргумент =", a);
+	return get_root();
+}
---
+++ linar5/linar5/root.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+#include "myexeption.h"
+#include <cmath>
+#include <ostream>
+/*
+* \brief Класса для вычесления корней квадратного уравнения
+*/
+class root
+{
+private:
+	/*
+	* brief коэф линейного уравнения
+	*/
+	double a;
+	double b;
+public:
+	/*
+	* \brief Конструктор с коэф уравнения
+	*/
+	root(const double a, const double b)
+	{
+		this->a = a;
+		this->b = b;
+	}
+	~root() = default;
+	/*
+	* Геттер для корня уравнения
+	*/
+	double get_root() const;
+	/*
+	* \brief Функция без спецификации исключений
+	*/
+	double noexep() const;
+	/*
+	* \brief Функция со спецификацией throw();
+	*/
+	double with_thow() throw();
+	/*
+	* \brief Функция с конкретной спецификацией с подходящим стандартным исключением
+	*/
+	double invalid_throw() throw(invalid_argument);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как пустой клас
+	*/
+	double empty() throw(empty_exeption);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как независимый класс с полями-параметрами функции
+	*/
+	double my_exp1() throw(my_exeption1);
+	/*
+	* \brief спецификация с собственным реализованным исключением: как наследник от стандартного исключения с полями
+	*/
+	double my_exp2() throw(my_exeption2);
+	/*
+	* \breif сравнение для типа double
+	*/
+	bool double_compare(const double value_a, const double value_b) const;
+};
